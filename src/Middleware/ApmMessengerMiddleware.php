7ea45fa8f5de79bb9c3f60c84064dab1577843c7
cristian.almohalla <cristian.almohalla@pccomponentes.com>
2021-11-26 19:24:17 +0100

    added apm middleware to trace messenger messages

---
+++ src/Middleware/ApmMessengerMiddleware.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace PcComponentes\SymfonyMessengerBundle\Middleware;
+
+use PcComponentes\Ddd\Util\Message\Message;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+
+final class ApmMessengerMiddleware implements MiddlewareInterface
+{
+    private const ELASTIC_APM_MESSAGE_TYPE = 'message';
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
+    {
+        $message = $envelope->getMessage();
+
+        if (false === $message instanceof Message || false === \class_exists('\Elastic\Apm\ElasticApm')) {
+            return $stack->next()->handle($envelope, $stack);
+        }
+
+        $currentTransaction = \Elastic\Apm\ElasticApm::getCurrentTransaction();
+
+        $parentDistributedTracingHeaders = [];
+        $currentTransaction->injectDistributedTracingHeaders(
+            function (string $headerName, string $headerValue) use (&$parentDistributedTracingHeaders): void {
+                $parentDistributedTracingHeaders[$headerName] = $headerValue;
+            }
+        );
+
+        $transaction = \Elastic\Apm\ElasticApm::newTransaction($message::messageName(), self::ELASTIC_APM_MESSAGE_TYPE)
+            ->distributedTracingHeaderExtractor(
+                function (string $headerName) use ($parentDistributedTracingHeaders): ?string {
+                    return $parentDistributedTracingHeaders[$headerName];
+                }
+            )
+            ->asCurrent()
+            ->begin();
+
+        try {
+            $envelope = $stack->next()->handle($envelope, $stack);
+        } finally {
+            $transaction->end();
+        }
+
+        return $envelope;
+    }
+}
